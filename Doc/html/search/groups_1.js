b76a1c0e67a1fda00109dc3751f7d4a6a5e01a56
Federico Marulli <federico.marulli3@unibo.it>
2018-10-02 23:26:11 +0200

    update

---
+++ Doc/html/search/groups_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['default_20values',['default values',['../d3/d27/group__default.html',1,'']]]
+];
---
+++ Doc/html/search/typedefs_1.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['functiondoubledouble',['FunctionDoubleDouble',['../d5/d33/namespacecbl.html#a76376dd2f772c0a70500eb1fcd0932c9',1,'cbl']]],
+  ['functiondoubledoubledoubleptrvectorref',['FunctionDoubleDoubleDoublePtrVectorRef',['../d5/d33/namespacecbl.html#a6be8f528e3622ee5a4781818ae2c021b',1,'cbl']]],
+  ['functiondoubledoubleptrvectorref',['FunctionDoubleDoublePtrVectorRef',['../d5/d33/namespacecbl.html#ab93abdc6ad0d8cb0855924beeb1c1432',1,'cbl']]],
+  ['functiondoublevector',['FunctionDoubleVector',['../d5/d33/namespacecbl.html#a1c255eb2d9516b922345f47bc387b91b',1,'cbl']]],
+  ['functiondoublevectorptrvectorref',['FunctionDoubleVectorPtrVectorRef',['../d5/d33/namespacecbl.html#ad0ba91b6791596e3f8a610f603f7d6d3',1,'cbl']]],
+  ['functiondoublevectorref',['FunctionDoubleVectorRef',['../d5/d33/namespacecbl.html#aba5d666d49c92f4887db71732a4db570',1,'cbl']]],
+  ['functionvectorvectorptrvectorref',['FunctionVectorVectorPtrVectorRef',['../d5/d33/namespacecbl.html#a502e51432af3d382446c7f5b658eeb07',1,'cbl']]]
+];
