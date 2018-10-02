4f6bc18f72f3b235e5357ef265768b8eb0b7f901
Federico Marulli <federico.marulli3@unibo.it>
2018-10-02 23:42:46 +0200

    update

--- Doc/html/search/files_4.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['eisensteinhu_2eh',['EisensteinHu.h',['../d6/d32/EisensteinHu_8h.html',1,'']]],
-  ['enumcast_2eh',['EnumCast.h',['../d5/d76/EnumCast_8h.html',1,'']]],
-  ['exception_2eh',['Exception.h',['../d8/d8a/Exception_8h.html',1,'']]]
-];
