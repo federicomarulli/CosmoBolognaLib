4faf785c8ad02389fe8d8e311c0750d58145b31d
Federico Marulli <federico.marulli3@unibo.it>
2018-10-02 22:47:54 +0200

    update

--- Doc/html/d8/df6/classcbl_1_1pairs_1_1Pair2D__comovingPolar__loglog__extra.js
+++
@@ -1,8 +0,0 @@
-var classcbl_1_1pairs_1_1Pair2D__comovingPolar__loglog__extra =
-[
-    [ "Pair2D_comovingPolar_loglog_extra", "d8/df6/classcbl_1_1pairs_1_1Pair2D__comovingPolar__loglog__extra.html#a6c8d76a314d8f3abd84dd868dacffced", null ],
-    [ "Pair2D_comovingPolar_loglog_extra", "d8/df6/classcbl_1_1pairs_1_1Pair2D__comovingPolar__loglog__extra.html#aefcfb31d92562f48dfc17fa462787bd5", null ],
-    [ "Pair2D_comovingPolar_loglog_extra", "d8/df6/classcbl_1_1pairs_1_1Pair2D__comovingPolar__loglog__extra.html#a305b5e25b959748e2d6c5262e26c79b1", null ],
-    [ "~Pair2D_comovingPolar_loglog_extra", "d8/df6/classcbl_1_1pairs_1_1Pair2D__comovingPolar__loglog__extra.html#ae7a3b7ae81a97cd54316145caa580945", null ],
-    [ "put", "d8/df6/classcbl_1_1pairs_1_1Pair2D__comovingPolar__loglog__extra.html#a8f44674e8a18aaf41a57afbf596f6a96", null ]
-];
--- Doc/html/db/d16/classcbl_1_1pairs_1_1Pair1D__comoving__multipoles__extra.js
+++
@@ -1,7 +0,0 @@
-var classcbl_1_1pairs_1_1Pair1D__comoving__multipoles__extra =
-[
-    [ "Pair1D_comoving_multipoles_extra", "db/d16/classcbl_1_1pairs_1_1Pair1D__comoving__multipoles__extra.html#a3cfc08101f30e558fd7d8049d5370c17", null ],
-    [ "Pair1D_comoving_multipoles_extra", "db/d16/classcbl_1_1pairs_1_1Pair1D__comoving__multipoles__extra.html#afea2aeaeb478bf0da4c87448e8de142a", null ],
-    [ "Pair1D_comoving_multipoles_extra", "db/d16/classcbl_1_1pairs_1_1Pair1D__comoving__multipoles__extra.html#ac5528db576561722b876b59024009751", null ],
-    [ "~Pair1D_comoving_multipoles_extra", "db/d16/classcbl_1_1pairs_1_1Pair1D__comoving__multipoles__extra.html#a83f249d5bf5836f029bf2377e876c258", null ]
-];
