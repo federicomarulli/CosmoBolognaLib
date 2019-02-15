4e217fefcd2c03300cd1dbe2381fc3e17c0dc227
Federico Marulli <federico.marulli3@unibo.it>
2019-02-15 08:54:55 +0100

    update

--- Doc/html/dd/db4/ModelFunction__ThreePointCorrelation__comoving__reduced_8h.js
+++
@@ -1,6 +0,0 @@
-var ModelFunction__ThreePointCorrelation__comoving__reduced_8h =
-[
-    [ "Q_nonlinear_localbias", "dd/db4/ModelFunction__ThreePointCorrelation__comoving__reduced_8h.html#a1ae89e2341cf5a4d304b5bc0b4019a32", null ],
-    [ "Q_nonlinear_nonlocalbias", "dd/db4/ModelFunction__ThreePointCorrelation__comoving__reduced_8h.html#a44828541c8a396f6a68cc066ddee645c", null ],
-    [ "Q_nonlinear_nonlocalbias_alpha", "dd/db4/ModelFunction__ThreePointCorrelation__comoving__reduced_8h.html#a41a46e9b06cd930ac36430f2e9baff13", null ]
-];
--- Doc/html/search/typedefs_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['svector4d',['sVector4D',['../d5/d33/namespacecbl.html#a616758aad255969ea259994ee1747585',1,'cbl']]]
-];
