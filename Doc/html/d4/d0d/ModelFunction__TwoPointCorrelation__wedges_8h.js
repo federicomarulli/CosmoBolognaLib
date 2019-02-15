2f06f3a71012a50fc7830857543ca6330509df2e
Federico Marulli <federico.marulli3@unibo.it>
2019-02-15 19:16:42 +0100

    update

--- Doc/html/d4/d0d/ModelFunction__TwoPointCorrelation__wedges_8h.js
+++
@@ -1,5 +0,0 @@
-var ModelFunction__TwoPointCorrelation__wedges_8h =
-[
-    [ "xiWedges", "d4/d0d/ModelFunction__TwoPointCorrelation__wedges_8h.html#a7458c8b2fd704ebd772f266977978fa1", null ],
-    [ "xiWedges_BAO", "d4/d0d/ModelFunction__TwoPointCorrelation__wedges_8h.html#a8d0ed2487d1f2f874ad6e072ee24ef03", null ]
-];
--- Doc/html/search/groups_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['physical_20and_20astronomical_20constants',['physical and astronomical constants',['../db/d1b/group__phys.html',1,'']]]
-];
