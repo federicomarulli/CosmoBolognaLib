c4432ddd32b302b626faec33bb65568aee3ead92
Alfonso Veropalumbo <alfonso.veropalumbo@uniroma3.it>
2020-04-16 14:31:53 +0200

    Fix bug in test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import os
 import sys
@@ -215,8 +215,8 @@ check("cosmicVoids/codes", "sizeFunction.py", "python3")
 check("cosmicVoids/codes", "cleanVoidCatalogue.py", "python", "../input/parameter_file.ini") 
 check("cosmicVoids/codes", "cleanVoidCatalogue.py", "python3", "../input/parameter_file.ini")
 
-check("readParameterFile", "parameter_file", "python") 
-check("readParameterFile", "parameter_file", "python3") 
+check("readParameterFile", "parameter_file.py", "python") 
+check("readParameterFile", "parameter_file.py", "python3") 
 
 ####################################################
 
